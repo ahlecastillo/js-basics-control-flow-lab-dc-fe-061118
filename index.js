0b6bebfdf3124070f4dadf038027f0fc602c2e97
Alejandra Castillo <ahle.castillo@gmail.com>
2018-07-24 00:41:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ function scuberGreetingForFeet(someValue) {
   }
 }
 
-function ternaryCheckCity(city){
-+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
-
+function ternaryCheckCity(city) {
+return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+}
 
 function switchOnCharmFromTip() {
   

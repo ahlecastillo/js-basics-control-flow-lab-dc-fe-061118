124409d8994402b34c9bf055453027735688bf70
Alejandra Castillo <ahle.castillo@gmail.com>
2018-07-24 00:55:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ function switchOnCharmFromTip(tip){
   case: "generous"
    return: "Thank you so much."
   break;
-  case: "not as generous"
-   return: "Thank you." 
+   case: "not as generous"
+    return: "Thank you." 
   break;
-  
+   case: ""
   
  
 }

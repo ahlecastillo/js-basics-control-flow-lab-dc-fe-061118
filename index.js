4143b6695333ea99548fa3ac10f04ed4ada3d722
Alejandra Castillo <ahle.castillo@gmail.com>
2018-07-24 01:01:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 
 function switchOnCharmFromTip(tip){
  switch (tip) {
-  case: "generous"
-   return "Thank you so much.";
+  case "generous":
+   return "Thank you so much.":
   break;
    case "not as generous"
     return "Thank you." 
